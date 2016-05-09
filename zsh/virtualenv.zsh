ed7a2a3936f92f4c4c5a465a84bb38a2aad45bae
Alexander Gavrilenko <gavrilea@advisory.com>
2016-05-09 16:34:50 -0400

    Virtualenv settings

--- zsh/virtualenv.zsh
+++ zsh/virtualenv.zsh
@@ -1,6 +1,6 @@
 # Virtualenv and virtualenvwrapper setup
-if [[ -f /opt/python/current/bin/virtualenvwrapper.sh ]]; then
-#    export PROJECT_HOME=~/lib/virtualenvs
+if [[ -f /usr/local/bin/virtualenvwrapper.sh ]]; then
+    export PROJECT_HOME=~/lib/virtualenvs
     export WORKON_HOME=${HOME}/Code
     export VIRTUALENVWRAPPER_HOOK_DIR=${HOME}/lib/virtualenvs
     export VIRTUALENVWRAPPER_LOG_DIR=${HOME}/lib/virtualenvs
@@ -8,9 +8,9 @@ if [[ -f /opt/python/current/bin/virtualenvwrapper.sh ]]; then
     export PIP_RESPECT_VIRTUALENV=true
     export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
     export VIRTUALENV_PROJECT_GIT_ROOT=${HOME}/Code/repos
-	export VIRTUALENVWRAPPER_PYTHON="/opt/python/current/bin/python2.7"
-	export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages --distribute'
-    source /opt/python/current/bin/virtualenvwrapper.sh
+    export VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python2.7"
+    export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages --distribute'
+    source /usr/local/bin/virtualenvwrapper.sh
 fi
 
 
