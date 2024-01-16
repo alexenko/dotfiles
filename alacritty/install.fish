043bcd36111689c6abfc6b1ca9a1baacd7514c57
Alexander Gavrilenko <1629938+alexenko@users.noreply.github.com>
2024-01-16 11:26:15 -0500

    Update alacritty configs to use toml, add vscode shell starter for fish

--- alacritty/install.fish
+++ alacritty/install.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
-curl -sL https://raw.githubusercontent.com/catppuccin/alacritty/main/catppuccin-macchiato.yml -o $HOME/lib/dotfiles/alacritty/catppuccin.yml
-link_file $DOTFILES_ROOT/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml backup
+curl -sL https://raw.githubusercontent.com/catppuccin/alacritty/main/catppuccin-macchiato.toml -o $HOME/lib/dotfiles/alacritty/catppuccin.toml
+link_file $DOTFILES_ROOT/alacritty/alacritty.toml $HOME/.config/alacritty/alacritty.toml backup
 	or abort alacritty
