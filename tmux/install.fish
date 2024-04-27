47a22c63b90bbd87278658de5b8663987c67ca7e
Alexander Gavrilenko <1629938+alexenko@users.noreply.github.com>
2024-04-27 16:48:42 -0400

    Try to restart tmux session and add vim copy/paste

--- tmux/install.fish
+++ tmux/install.fish
@@ -4,7 +4,7 @@ end
 
 test -d ~/.tmux/plugins/tpm || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-alias --save ta='tmux-new'
+alias --save ta='tmux new-session -A -s main'
 
 link_file $DOTFILES_ROOT/tmux/tmux.conf $HOME/.tmux.conf
 	or abort tmux
