10902be5d5f2a39350ed645c9d3a1de30dcc140b
Alexander Gavrilenko <agavrilenko@eab.com>
2025-02-08 23:46:54 -0500

    Cleanup project

--- zsh/virtualenv.zsh
+++
@@ -1,7 +0,0 @@
-# Virtualenv and virtualenvwrapper setup
-export PROJECT_HOME=${HOME}/code
-export WORKON_HOME=${HOME}/.virtualenvs
-export VIRTUALENVWRAPPER_HOOK_DIR=${HOME}/lib/dotfiles/virtualenvwrapper
-export PYENV_VIRTUALENVWRAPPER_PREFER_PYENV="true"
-pyenv virtualenvwrapper
- 
